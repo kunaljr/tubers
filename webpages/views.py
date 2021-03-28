7b29faa759056e975d4599f222cb09eb9389a389
kunal <kunalshelke123@gmail.com>
2021-03-28 07:39:33 +0530

    second commit

--- webpages/views.py
+++ webpages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    pass
+    return HttpResponse("<html><body>It is now %s.</body></html>")
 
 def about(request):
     pass
